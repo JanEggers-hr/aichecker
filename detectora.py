341f2089e49236e014ea2dad8280d359301ca2e7
Jan Eggers <janeggers@untergeekPro.local>
2024-12-25 13:30:48 +0100

    Detectora angefangen

---
+++ detectora.py
@@ -0,0 +1,7 @@
+# detectora.py Library - CC-BY github.com/JanEggers-hr
+"""
+Eine Python-Bibliothek, um mit detectora.de auf KI-generierten Text zu prüfen. 
+
+Detectora-Key muss als DETECTORA_API_KEY in .env hinterlegt sein. 
+"""
+
