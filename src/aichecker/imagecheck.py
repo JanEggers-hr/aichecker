8b61d3981d939706ebad7cd549b80a9474acfb3c
Jan Eggers <janeggers@untergeekPro.local>
2024-12-31 11:45:36 +0100

    V0.1.3

--- src/aichecker/imagecheck.py
+++ src/aichecker/imagecheck.py
@@ -1,6 +1,6 @@
 # imagecheck.py
 # Erfragt KI-Wahrscheinlichkeit für ein Bild über Hive- und AIorNot-API
-from src.bildbeschreibung import ai_description
+from .bildbeschreibung import ai_description
 
 import requests
 import json
