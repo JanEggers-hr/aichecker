e13892e7edc0cfb2ecf3567979e3ce2303652c39
Jan Eggers <janeggers@untergeekPro.local>
2025-02-04 17:20:22 +0100

    Added to __init__.py

--- src/aichecker/__init__.py
+++ src/aichecker/__init__.py
@@ -4,5 +4,5 @@ from .detectora import query_detectora
 from .je_aiornot import query_aiornot
 from .check_wrappers import aiornot_wrapper, detectora_wrapper, bsky_aiornot_wrapper, hive_visual, hive_visual_async
 from .check_tg import tgc_clean, tgc_read, tgc_blockread, tgc_read_url, tgc_profile, tgc_read_range, tgc_read_number, tg_evaluate, tg_hydrate
-from .check_ig import igc_profile, igc_read_posts, igc_read_posts_until, ig_evaluate, ig_hydrate, ig_reimport_csv, ig_append_csv, igc_clean
+from .check_ig import igc_profile, igc_read_posts, igc_read_posts_until, ig_evaluate, ig_hydrate, ig_reimport_csv, ig_append_csv, igc_clean, igc_read_stories, igc_read_highlights
 from .save_urls import save_url, save_url_async
